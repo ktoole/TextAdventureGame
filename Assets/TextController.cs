55f2b96ad10f423e640b3fc39dc3cc2878b89b4d
Kristian Toole <kristiantoole20@gmail.com>
2017-10-03 20:10:19 -0400

    Added 2D interface UI

---
+++ Assets/TextController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextController : MonoBehaviour {
+
+    public Text text;
+
+	// Use this for initialization
+	void Start () {
+
+        text.text = "Hello, World";
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown("space")) {
+            text.text = "Space Key Pressed";
+        }
+    }
+}
